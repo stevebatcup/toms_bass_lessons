65fb3db8f386eb4b4fdef7b94b1cad810fe3c013
Steven Batcup <steve.batcup@gmail.com>
2020-04-23 11:24:17 +0100

    added exception notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+    mandrill: {
+      subject: "Exception on Toms Bass Lessons",
+      recipient_address: "developer@maawol.com"
+    }
 end
