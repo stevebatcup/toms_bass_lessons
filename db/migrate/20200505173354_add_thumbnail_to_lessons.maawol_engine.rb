bc0e21cf42ee8de56abb131841aa9c4912e76c07
Steven Batcup <steve.batcup@gmail.com>
2020-05-08 11:33:37 +0100

    removed dodgy migration

--- db/migrate/20200505173354_add_thumbnail_to_lessons.maawol_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from maawol_engine (originally 20200428123845)
-class AddThumbnailToLessons < ActiveRecord::Migration[6.0]
-  def change
-    add_column :lessons, :thumbnail, :string
-  end
-end
